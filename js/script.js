25d265b87efda092b98422ba4aadbb6cc5495739
Andrea <grassiandrea95@gmail.com>
2020-11-18 17:19:17 +0100

    casi limite

--- js/script.js
+++ js/script.js
@@ -6,11 +6,21 @@ var app=new Vue({
     list:[]
   },
   methods:{
-    add:function () {
-      this.list.push(this.itemList);
-      this.itemList='';
 
+    add:function () {
+      
+      this.itemList = this.itemList.substr(0,1).toUpperCase()+this.itemList.substr(1,this.itemList.length).toLowerCase();
+
+      if (this.itemList=='') {
+        alert('attenzione inserisci un elemento');
+      }else if (this.list.includes(this.itemList)) {
+        alert('elemento già inserito');
+      }else {
+        this.list.push(this.itemList);
+      }
+      this.itemList='';
     },
+
     remove:function (index) {
        this.list.splice(index,1);
     }
