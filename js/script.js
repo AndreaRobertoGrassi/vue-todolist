04d6a9812a69d922737243ece5e3ff862d96c828
Andrea <grassiandrea95@gmail.com>
2020-11-18 19:20:25 +0100

    modifiche generali

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ var app=new Vue({
     list:[]
   },
   methods:{
-
-    add:function () {
-      
+    
+    //funzione per aggiungere un elemento tramite tastiera
+    enter:function () {
+      // prima lettera maiuscola
       this.itemList = this.itemList.substr(0,1).toUpperCase()+this.itemList.substr(1,this.itemList.length).toLowerCase();
 
-      if (this.itemList=='') {
+      if (this.itemList=='') { //se non inserisco niente
         alert('attenzione inserisci un elemento');
-      }else if (this.list.includes(this.itemList)) {
+      }else if (this.list.includes(this.itemList)) { //se l'input è gia stato inserito
         alert('elemento già inserito');
       }else {
         this.list.push(this.itemList);
@@ -21,6 +22,22 @@ var app=new Vue({
       this.itemList='';
     },
 
+    //funzione per aggiungere un elemento
+    add:function () {
+      // prima lettera maiuscola
+      this.itemList = this.itemList.substr(0,1).toUpperCase()+this.itemList.substr(1,this.itemList.length).toLowerCase();
+
+      if (this.itemList=='') { //se non inserisco niente
+        alert('attenzione inserisci un elemento');
+      }else if (this.list.includes(this.itemList)) { //se l'input è gia stato inserito
+        alert('elemento già inserito');
+      }else {
+        this.list.push(this.itemList);
+      }
+      this.itemList='';
+    },
+
+    //funzione per eliminare l'elemento selezionato
     remove:function (index) {
        this.list.splice(index,1);
     }
