02390681e4a897d3aa5acfaf93e3ee30752c1019
Andrea <grassiandrea95@gmail.com>
2020-11-18 21:29:03 +0100

    aggiunto funzione removeAll

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
 var app=new Vue({
   el:'#app',
   data:{
+
     logo:'img/logo.png',
     itemList:'',
-    list:[]
-  },
-  methods:{
-    
-    //funzione per aggiungere un elemento tramite tastiera
-    enter:function () {
-      // prima lettera maiuscola
-      this.itemList = this.itemList.substr(0,1).toUpperCase()+this.itemList.substr(1,this.itemList.length).toLowerCase();
-
-      if (this.itemList=='') { //se non inserisco niente
-        alert('attenzione inserisci un elemento');
-      }else if (this.list.includes(this.itemList)) { //se l'input è gia stato inserito
-        alert('elemento già inserito');
-      }else {
-        this.list.push(this.itemList);
-      }
-      this.itemList='';
-    },
+    list:[],
 
     //funzione per aggiungere un elemento
-    add:function () {
+    addItem:function () {
       // prima lettera maiuscola
       this.itemList = this.itemList.substr(0,1).toUpperCase()+this.itemList.substr(1,this.itemList.length).toLowerCase();
 
@@ -34,12 +18,30 @@ var app=new Vue({
       }else {
         this.list.push(this.itemList);
       }
-      this.itemList='';
+      this.itemList=''
+    }
+  },
+
+  methods:{
+
+    //funzione per aggiungere un elemento tramite tastiera
+    addItemKey:function () {
+      this.addItem();
+    },
+
+    //funzione per aggiungere un elemento tramite button
+    addItemButton:function () {
+      this.addItem();
     },
 
     //funzione per eliminare l'elemento selezionato
     remove:function (index) {
        this.list.splice(index,1);
+    },
+
+    //funzione per rimuovere cancellare tutta la lista
+    removeAll:function () {
+      this.list=[];
     }
   }
 });
